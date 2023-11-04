0ba23e24c553c8eab6e7906620c54829836ef15d
BhaveshC07 <bhaveshcharde4321@gmail.com>
2023-11-04 15:41:13 +0530

    file add

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+  // printf("Hello, World!");
+   int num;
+   scanf("%d",&num);
+   printf("%d",num);
+   return 0;
+}
